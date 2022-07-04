064ce23368a6569f48eb4002569af300eb854e18
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-07-04 17:49:15 +0100

    Initial Commit

---
+++ FootballChants/FootballChantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballChantsApp.swift
+//  FootballChants
+//
+//  Created by GB on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FootballChantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
