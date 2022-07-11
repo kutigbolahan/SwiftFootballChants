c9e2845ecb3d32fd6e7b4f78796d41b0bea88136
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2022-07-11 11:32:20 +0100

    added the badges

--- FootballChants/ContentView.swift
+++ FootballChants/ContentView.swift
@@ -11,23 +11,10 @@ struct ContentView: View {
     let team: Team
     var body: some View {
       
-            VStack {
-                Text("Hello, world!")
-                    .padding()
-              
-                    Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    Form {
-                        Text("Content")
-                 
-                    .searchable(text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=$text@*/.constant("")/*@END_MENU_TOKEN@*/, placement: /*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
-                }
-               
-            
-          
+        VStack(alignment: .leading){
+            HStack(alignment: .top, spacing: 12){
+                Image(team.id.badge)
+            }
         }
     }
 }
